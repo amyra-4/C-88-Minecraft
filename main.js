cf7edd3761bf11085888a3bd827bfce518fa863d
amyra-4 <88983212+amyra-4@users.noreply.github.com>
2022-02-05 14:50:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,6 +30,15 @@ player_x = 10;
            canvas.add(player_object);
          }); }
 
+         function new_image(get_image) { 
+           fabric.Image.fromURL(get_image, function(Img) {
+              block_image_object = Img;
+           block_image_object.scaleToWidth(block_image_width);
+            block_image_object.scaleToHeight(block_image_height);
+           block_image_object.set({ top:player_y, left:player_x
+           });
+            canvas.add(block_image_object); }); 
+          }
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e) {
